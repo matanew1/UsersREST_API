e0b0c4bacb24c6052e77e8a9a3a9f424a905eac5
matanew1 <matan.bardugo@s.afeka.ac.il>
2023-05-07 15:06:08 +0300

    add closing tab/browser -> logout user admin -> expires session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './components/profile/Profile';
 import Home from './components/home/Home';
@@ -16,6 +17,33 @@ import Authentication from './components/login/Authentication'
 import SignUpPage from './components/login/SignUpPage';
 
 function App() {
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      // Make a fetch request to /api/logout
+      fetch('/api/logout', {
+        method: 'GET',
+      }).then(response => {
+        response.json().then(data => console.log(data))
+        if (response.ok) {
+          console.log("Admin logged out")
+        } else {
+          throw new Error('Error logout admin');
+        }
+      });
+
+      // The following line is optional, it shows a confirmation dialog to the user
+      event.preventDefault();
+      event.returnValue = ''; // Some browsers require a non-empty string
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <AuthProvider>
       <BrowserRouter>
